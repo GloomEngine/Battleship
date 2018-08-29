04e8886f68407360256286339d07f440cf362ba6
Kyle <kyle.sander102@gmail.com>
2018-08-29 11:47:29 -0500

    Completed Setname Function

--- battleship.cpp
+++ battleship.cpp
@@ -3,19 +3,47 @@
 #include "battleship_class.cpp"
 using namespace std;
 
-void setname(battleship*);
+void setname(battleship*, int);
 
 int main(void)
 {
 
+    size_t player_count = 2;
     battleship player[2];
     battleship *player_pointer = player;
 
-    setname(player_pointer);
+    setname(player_pointer, player_count);
 
 }
 
-void setname(battleship *player)
+void setname(battleship *player, int size)
 {
+
+    string name;    
+    
+    for(int x = 0; x < size; x++)
+    {
+        cout << "Enter Player[" << x + 1 << "] name: ";
+        getline(cin, name);
+        
+        while(name.empty() || !isalpha(name.at(0)))
+        {
+            if(name.empty())
+            {
+                cout << "\nError: Name cannot be empty\n";
+                cout << "Enter Player[" << x + 1 << "] name: ";
+            }
+            else
+            {
+                cout << "\nError: Name must start with a letter\n";
+                cout << "Enter Player[" << x + 1 << "] name: ";
+            }
+            getline(cin, name);
+        }
+
+        player[x].setname(name);
+        cout << endl;
+    }
+    
     
 }
